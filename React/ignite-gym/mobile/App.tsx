db5a91437c2869b60cc0d2e8522a1a2cc2678c42
JoaoGarbin <garbinjoaovitor@gmail.com>
2025-03-14 11:26:15 -0300

    Ignite team Api

---
+++ React/ignite-gym/mobile/App.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from '@expo-google-fonts/roboto';
+
+import { AuthContextProvider } from './src/contexts/AuthContext';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { config } from './config/gluestack-ui.config';
+import { Loading } from '@components/Loading';
+import { Routes } from '@routes/index';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
+  return (
+    <GluestackUIProvider config={config}>
+
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <AuthContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
+
+    </GluestackUIProvider>
+  );
+}
---
+++ React/ignite-gym/mobile/src/screens/SingIn.tsx
@@ -0,0 +1,117 @@
+import { VStack, Image, Center, Text, Heading, ScrollView, useToast } from "@gluestack-ui/themed"
+import { Input } from "@components/input"
+
+import BackgroundImg from "@assets/background.png"
+import Logo from "@assets/logo.svg"
+import { Button } from "@components/Button"
+import { AuthNavigatorRoutesProps } from "../routes/auth.routes"
+import { useNavigation } from "@react-navigation/native"
+import { Controller, useForm } from "react-hook-form"
+
+import { useAuth } from "@hooks/useAuth"
+import { AppError } from "@utils/AppError"
+
+type FormData = {
+    email: string;
+    password: string;
+}
+
+export function SingIn() {
+    const { singIn } = useAuth();
+    const toast = useToast();
+
+    const navigation = useNavigation<AuthNavigatorRoutesProps>()
+
+    const { control, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+    function handleNewAccount() {
+        navigation.navigate("signUp")
+    }
+
+    async function handleSingIn({ email, password }: FormData) {
+        try {
+            await singIn(email, password);
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+
+            const title = isAppError ? error.message : "Não foi possivel acessar. tente novamente mais tarde"
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        }
+    }
+    return (
+        <ScrollView
+            contentContainerStyle={{ flexGrow: 1 }}
+            showsVerticalScrollIndicator={false}>
+            <VStack flex={1}>
+                <Image
+                    w="$full"
+                    h={624}
+                    source={BackgroundImg}
+                    defaultSource={BackgroundImg}
+                    position="absolute"
+                    alt="Pessoas Treinando"
+                />
+                <VStack flex={1} px="$10" pb="$16">
+                    <Center my="$24">
+                        <Logo />
+
+                        <Text color="$gray100" fontSize="$sm">
+                            treine sua mente e o seu corpo
+                        </Text>
+                    </Center>
+
+                    <Center gap="$2">
+                        <Heading color="$gray100">Acesse a conta</Heading>
+
+                        <Controller
+                            control={control}
+                            name="email"
+                            rules={{ required: "Informe o e-mail" }}
+                            render={({ field: { onChange } }) => (
+                                <Input
+                                    placeholder="E-mail"
+                                    keyboardType="email-address"
+                                    onChangeText={onChange}
+                                    errorMessage={errors.email?.message}
+                                    autoCapitalize="none"
+                                />
+                            )}
+                        />
+
+                        <Controller
+                            control={control}
+                            name="password"
+                            rules={{ required: "Informe a sua Senha" }}
+                            render={({ field: { onChange } }) => (
+                                <Input
+                                    secureTextEntry
+                                    placeholder="Senha"
+                                    onChangeText={onChange}
+                                    errorMessage={errors.password?.message}
+                                />
+                            )}
+                        />
+
+                        <Button
+                            title="Acessar"
+                            onPress={handleSubmit(handleSingIn)} />
+                    </Center>
+
+                    <Center flex={1} justifyContent="flex-end" mt="$4">
+                        <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">Ainda não tem acesso?</Text>
+
+                        <Button title="Criar Conta" variant="outline" onPress={handleNewAccount} />
+                    </Center>
+
+
+                </VStack>
+
+            </VStack>
+        </ScrollView>
+    )
+}
