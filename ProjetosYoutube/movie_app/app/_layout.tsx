b294f449579545eb4b45ae040e091d44d02a3e29
JoaoGarbin <garbinjoaovitor@gmail.com>
2025-05-02 09:24:55 -0300

    StatusBar movie app

--- ProjetosYoutube/movie_app/app/_layout.tsx
+++ ProjetosYoutube/movie_app/app/_layout.tsx
@@ -1,24 +1,31 @@
 import { Stack } from "expo-router";
 import './globals.css';
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack>
-
-      <Stack.Screen
-        name="(tabs)"
-        options={{
-          headerShown: false,
-        }}
+    <>
+      <StatusBar
+        translucent={true}
+        backgroundColor="transparent"
+        barStyle="light-content"
       />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
 
-      <Stack.Screen
-        name="movie/[id]"
-        options={{
-          headerShown: false,
-        }}
-      />
+        <Stack.Screen
+          name="movie/[id]"
+          options={{
+            headerShown: false,
+          }}
+        />
 
-    </Stack>
+      </Stack>
+    </>
   )
 }
