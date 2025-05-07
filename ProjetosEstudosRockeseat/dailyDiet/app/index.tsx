e228f04d2c660171f1366f0d6198c0d5b9dbdd0d
JoaoGarbin <garbinjoaovitor@gmail.com>
2025-05-07 08:29:00 -0300

    DailyDiet fist commit

---
+++ ProjetosEstudosRockeseat/dailyDiet/app/index.tsx
@@ -0,0 +1,17 @@
+import { Image, Text, View } from "react-native";
+import '../global.css';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+
+      <Text>Daily Diet</Text>
+    </View>
+  );
+}
