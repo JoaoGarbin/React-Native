dafcaa160d8dac6249703aafd74a5c6c56edf52a
JoaoGarbin <garbinjoaovitor@gmail.com>
2025-05-22 11:01:10 -0300

    Explore real-estate-app

--- ProjetosYoutube/real-estate-app/app/(root)/(tabs)/explore.tsx
+++ ProjetosYoutube/real-estate-app/app/(root)/(tabs)/explore.tsx
@@ -1,12 +1,83 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import NoResults from "@/components/NoResults";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import { getLatestProperties, getProperties } from "@/constants/lib/appwrite";
+import { useGlobalContext } from "@/constants/lib/global-provider";
+import seed from "@/constants/lib/seed";
+import { useAppwrite } from "@/constants/lib/useAppwrite";
+import { Link, router, useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, Button, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const explore = () => {
+export default function Explore() {
+    const params = useLocalSearchParams<{ query?: string, filter?: string; }>();
+
+
+    const { data: properties, loading, refetch } = useAppwrite({
+        fn: getProperties,
+        params: {
+            filter: params.filter!,
+            query: params.query!,
+            limit: 6,
+        },
+        skip: true,
+    });
+
+    const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+
+    useEffect(() => {
+        refetch({
+            filter: params.filter!,
+            query: params.query!,
+            limit: 20,
+        })
+    }, [params.filter, params.query])
+
     return (
-        <View>
-            <Text>explore</Text>
-        </View>
-    )
-}
+        <SafeAreaView className="bg-white h-full gap-5 px-2">
+            <FlatList
+                data={properties}
+                renderItem={({ item }) => <Card item={item} onPress={() => handleCardPress(item.$id)} />}
+                keyExtractor={(item) => item.$id}
+                numColumns={2}
+                contentContainerClassName="pb-32"
+                columnWrapperClassName="flex gap-5 px-3"
+                showsVerticalScrollIndicator={false}
+                ListEmptyComponent={
+                    loading ? (
+                        <ActivityIndicator size="large" className="text-primary-300 mt-5" />
+                    ) : <NoResults />
+                }
+                ListHeaderComponent={
+                    <View className="px-5">
+                        <View className="flex flex-row items-center justify-between mt-5">
+                            <TouchableOpacity
+                                onPress={() => router.back()}
+                                className="flex flex-row bg-primary-200 rounded-full size-11 items-center justify-center"
+                            >
+                                <Image source={icons.backArrow} className="size-5" />
+                            </TouchableOpacity>
+                            <Text className="text-base mr-2 text-center font-rubik-medium text-black-300">
+                                Search for Your Ideal Home
+                            </Text>
+                            <Image source={icons.bell} className="w-6 h-6" />
+                        </View>
+                        <Search />
 
-export default explore
+                        <View className="mt-5">
+                            <Filters />
+
+                            <Text className="text-xl font-rubik-bold text-black-300 mt-5">
+                                Found {properties?.length} Properties
+                            </Text>
+                        </View>
+                    </View>
+                }
+            />
+
+        </SafeAreaView>
+    );
+};
--- ProjetosYoutube/real-estate-app/components/Cards.tsx
+++ ProjetosYoutube/real-estate-app/components/Cards.tsx
@@ -26,7 +26,7 @@ export const FeaturedCard = ({ item: { image, rating, name, address, price }, on
 
                 <View className='flex flex-row items-center justify-between w-full'>
                     <Text className='text-xl font-rubik-extrabold text-white'>
-                        {price}
+                        ${price}
                     </Text>
                     <Image source={icons.heart} className='size-5' />
                 </View>
@@ -51,7 +51,7 @@ export const Card = ({ item: { image, rating, name, address, price }, onPress }:
 
                 <View className='flex flex-row items-center justify-between mt-2'>
                     <Text className='text-base font-rubik-extrabold text-primary-300'>
-                        {price}
+                        ${price}
                     </Text>
                     <Image source={icons.heart} className='w-5 h-5 mr-2' tintColor="#191d31" />
 
