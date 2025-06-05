607a9e0377e75f37c6ddae37059bf2fbb5203f8c
JoaoGarbin <garbinjoaovitor@gmail.com>
2025-06-05 11:01:45 -0300

    navigations bottom e Drawer

--- ProjetosEstudosRockeseat/navigation/src/app/Home.tsx
+++ ProjetosEstudosRockeseat/navigation/src/app/Home.tsx
@@ -4,12 +4,15 @@ import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
 import { ButtonIcon } from "@/components/ButtonIcon";
 import { StackRoutesProps } from "@/routes/StackRoutes";
+import { BottomRoutesProps } from "@/routes/BottomRoutes";
+import { DrawerRoutesProps } from "@/routes/DrawerRoutes";
 
-export function Home({ navigation }: StackRoutesProps<"home">) {
+export function Home({ navigation }: DrawerRoutesProps<"home">) {
 
     return (
         <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
             <Header>
+                <ButtonIcon name="menu" onPress={() => navigation.toggleDrawer()} />
                 <Title>Home</Title>
                 <ButtonIcon name="add-circle" onPress={() => navigation.navigate("product", { id: "1" })} />
             </Header>
